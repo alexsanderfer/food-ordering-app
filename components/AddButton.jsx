587f431a8e5b7f66b0b0dd4ac59cc2bf2d1ed3c7
AlexsanderFer <afgouveia90@gmail.com>
2022-07-15 02:19:47 +0100

    Initial commit 22

---
+++ components/AddButton.jsx
@@ -0,0 +1,11 @@
+import styles from "../styles/Add.module.css";
+
+const AddButton = ({ setClose }) => {
+  return (
+    <div onClick={() => setClose(false)} className={styles.mainAddButton}>
+      Adicionar nova Pizza
+    </div>
+  );
+};
+
+export default AddButton;
